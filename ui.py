8f73d4d87927b8d0860c165d1f9d20cf6e1a9df6
mwrzec <marcel.wrzecionek@gmail.com>
2023-12-09 14:35:53 -0500

    created main menu

---
+++ ui.py
@@ -0,0 +1,7 @@
+class UI:
+    def main_menu(self):
+        print("1. Create Resource")
+        print("2. Search Resource")
+        print("3. Edit Resource")
+        print("4. Delete Resource")
+        print("5. Exit")
